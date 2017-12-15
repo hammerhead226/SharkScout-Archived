2309b433a7b23ccd14045f36b5a63e3f5fd9ca24
Christian Emmer <emmercm@gmail.com>
2017-12-14 22:43:17 -0500

    build.bat now does a clean update and dump.

--- build.bat
+++ build.bat
@@ -11,11 +11,26 @@ del /F SharkScout.spec
 rmdir /S /Q __pycache__
 rmdir /S /Q build
 
-copy LICENSE dist\
+copy LICENSE* dist\
 copy README* dist\
 echo .rar\ > exclude
 xcopy www dist\www /S /V /I /Y /EXCLUDE:exclude
 del /F exclude
+
+cd dist
+for /f "skip=1 tokens=1-6" %%A in ('WMIC Path Win32_LocalTime Get Day^,Hour^,Minute^,Month^,Second^,Year /Format:table') do (
+    if "%%B" NEQ "" (
+        set /A FDATE=%%F*10000+%%D*100+%%A
+        set /A FTIME=%%B*10000+%%C*100+%%E
+    )
+)
+set YEAR_CURR=%FDATE:~0,4%
+set /A YEAR_NEXT=%FDATE:~0,4%+1
+SharkScout.exe -ut -uti -ue "%YEAR_CURR%-%YEAR_NEXT%" -uei "%YEAR_CURR%-%YEAR_NEXT%" --dump mongodump.gz
+if %errorlevel% neq 0 exit /b %errorlevel%
+ping localhost -n 4 >NUL
+rmdir /S /Q mongo
+cd ..
 
 for /f %%i in ('git rev-parse HEAD') do set HASH=%%i
 del /F SharkScout-%HASH:~0,7%-x86.zip
