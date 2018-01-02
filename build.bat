b6fd90f91bdc2e960207801430a4600934df6d0f
Christian Emmer <emmercm@gmail.com>
2018-01-01 20:36:24 -0500

    Use virtualenv for build.

--- build.bat
+++ build.bat
@@ -1,22 +1,46 @@
 cd "%~dp0"
 
-py SharkScout.py -h > nul
-if %errorlevel% neq 0 exit /b %errorlevel%
+:: Create virtualenv
+pip3 install --upgrade virtualenv
+rmdir /S /Q venv
+virtualenv venv
+venv\Scripts\pip3 install backoff cherrypy^<11 genshi psutil pymongo pynumparser requests tqdm ws4py
+venv\Scripts\pip3 install pyinstaller
 
-pip3 install --upgrade pyinstaller
+:: Test script
+venv\Scripts\python SharkScout.py -h > nul
+if %errorlevel% neq 0 (
+	rmdir /S /Q venv
+	exit /B 1
+)
+
+:: Run pyinstaller
 rmdir /S /Q dist
 for /f %%i in ('where python') do set PYTHON=%%i
-pyinstaller --noconfirm --clean --onefile --add-data "config.json;." --exclude-module matplotlib --exclude-module PyQt5 --icon "%PYTHON%,0" SharkScout.py
+venv\Scripts\pyinstaller --noconfirm --clean --onefile --add-data "config.json;." --icon "%PYTHON%,0" SharkScout.py
 del /F SharkScout.spec
 rmdir /S /Q __pycache__
 rmdir /S /Q build
+rmdir /S /Q venv
 
+:: Test compiled script
+cd dist
+SharkScout.exe -h
+if %errorlevel% neq 0 (
+	cd ..
+	rmdir /S /Q dist
+	exit /B 1
+)
+cd ..
+
+:: Copy files for distribution
 copy LICENSE* dist\
 copy README* dist\
 echo .rar\ > exclude
 xcopy www dist\www /S /V /I /Y /EXCLUDE:exclude
 del /F exclude
 
+:: Export TBA data for this year and next year
 cd dist
 for /f "skip=1 tokens=1-6" %%A in ('WMIC Path Win32_LocalTime Get Day^,Hour^,Minute^,Month^,Second^,Year /Format:table') do (
     if "%%B" NEQ "" (
@@ -27,11 +51,16 @@ for /f "skip=1 tokens=1-6" %%A in ('WMIC Path Win32_LocalTime Get Day^,Hour^,Min
 set YEAR_CURR=%FDATE:~0,4%
 set /A YEAR_NEXT=%FDATE:~0,4%+1
 SharkScout.exe -ut -uti -ue "%YEAR_CURR%-%YEAR_NEXT%" -uei "%YEAR_CURR%-%YEAR_NEXT%" --dump mongodump.gz
-if %errorlevel% neq 0 exit /b %errorlevel%
+if %errorlevel% neq 0 (
+	cd ..
+	rmdir /S /Q dist
+	exit /B 1
+)
 ping localhost -n 4 >NUL
 rmdir /S /Q mongo
 cd ..
 
+:: Make a distribution zip
 for /f %%i in ('git rev-parse HEAD') do set HASH=%%i
 del /F SharkScout-%HASH:~0,7%-x86.zip
 powershell -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory('dist', 'SharkScout-%HASH:~0,7%-x86.zip'); }"
